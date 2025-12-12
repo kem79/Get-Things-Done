788d439aab0242735ad83bf3c622bf118ad3397c
Marc <kemar79@qq.com>
2025-12-12 10:06:07 +0000

    add Tasks fragment and tab layout mediator #3

--- app/src/main/java/com/marcal/getthingsdone/MainActivity.kt
+++ app/src/main/java/com/marcal/getthingsdone/MainActivity.kt
@@ -5,10 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.marcal.getthingsdone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pager.adapter = PagerAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.pager) { tab, position ->
+            tab.text = "Tasks"
+        }
+    }
+
+    inner class PagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+        override fun createFragment(position: Int): Fragment {
+            return TasksFragment()
+        }
+
+        override fun getItemCount() = 1
+
     }
 }
