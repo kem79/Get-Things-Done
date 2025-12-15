6b994867102d4adebdc91f8a33cd577983098149
Marc <kemar79@qq.com>
2025-12-15 11:37:21 +0000

    Create task database #7

--- app/src/main/java/com/marcal/getthingsdone/MainActivity.kt
+++ app/src/main/java/com/marcal/getthingsdone/MainActivity.kt
@@ -2,15 +2,13 @@ package com.marcal.getthingsdone
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayoutMediator
+import com.marcal.getthingsdone.data.GetThingsDoneDatabase
 import com.marcal.getthingsdone.databinding.ActivityMainBinding
 import com.marcal.getthingsdone.databinding.DialogAddTaskBinding
 
@@ -29,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         }.attach()
 
         binding.fab.setOnClickListener { displayAddTaskDialog() }
+
+        val db = GetThingsDoneDatabase.createDatabase(this)
     }
 
     private fun displayAddTaskDialog() {
@@ -37,13 +37,17 @@ class MainActivity : AppCompatActivity() {
             .setTitle("Add new task")
             .setView(dialogBinding.root)
             .setPositiveButton("Save") { _, _ ->
-                Toast.makeText(this, "Your task is : ${dialogBinding.editText.text}", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "Your task is : ${dialogBinding.editText.text}",
+                    Toast.LENGTH_LONG
+                ).show()
             }
             .setNegativeButton("Cancel", null)
             .show()
     }
 
-    inner class PagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+    inner class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
         override fun createFragment(position: Int): Fragment {
             return TasksFragment()
         }
--- app/src/main/java/com/marcal/getthingsdone/data/GetThingsDoneDatabase.kt
+++ app/src/main/java/com/marcal/getthingsdone/data/GetThingsDoneDatabase.kt
@@ -1,9 +1,21 @@
 package com.marcal.getthingsdone.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Task::class], version = 1)
 abstract class GetThingsDoneDatabase : RoomDatabase() {
     abstract fun getTaskDao(): TaskDao
+
+    companion object {
+        fun createDatabase(context: Context): GetThingsDoneDatabase {
+            return Room.databaseBuilder(
+                context,
+                GetThingsDoneDatabase::class.java,
+                "get-things-done"
+            ).build()
+        }
+    }
 }
